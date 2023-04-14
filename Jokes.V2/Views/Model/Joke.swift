ca35da2b70733ac33887367a242744092a263971
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-14 12:16:26 -0400

    Made Joke file and made Joke structure.

---
+++ Jokes.V2/Views/Model/Joke.swift
@@ -0,0 +1,15 @@
+//
+//  Joke.swift
+//  Jokes.V2
+//
+//  Created by Cooper Lawson on 2023-04-14.
+//
+
+import Foundation
+
+struct Joke: Identifiable {
+    let type: String
+    let setup: String
+    let punchline: String
+    let id: Int
+}
