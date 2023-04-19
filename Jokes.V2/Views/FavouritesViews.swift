79dbfa617380a35447a499b8dc7c80a1166501ab
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-19 10:45:07 -0400

    Now reading contents of Joke table and displaying all rows found in a list.

--- Jokes.V2/Views/FavouritesViews.swift
+++ Jokes.V2/Views/FavouritesViews.swift
@@ -5,16 +5,33 @@
 //  Created by Cooper Lawson on 2023-04-19.
 //
 
+import Blackbird
 import SwiftUI
 
 struct FavouritesViews: View {
+    
+    // MARK Stored Properties
+    
+    @BlackbirdLiveModels ({ db in
+        try await Joke.read(from: db)
+    }) var favouriteJokes
+    
+    // MARK Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(favouriteJokes.results) {currentJoke in
+            VStack(alignment: .leading) {
+                Text(currentJoke.setup)
+                    .bold()
+                Text(currentJoke.punchline)
+            }
+        }
     }
 }
 
 struct FavouritesViews_Previews: PreviewProvider {
     static var previews: some View {
         FavouritesViews()
+        // Make the database available to this view in Xcode Previews
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
