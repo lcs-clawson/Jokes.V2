d5701ada03f9362fd3fa1cf1d8bd880cc2f6edfe
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-19 10:32:06 -0400

    Started to add view to see the list of saved / favourite jokes.

---
+++ Jokes.V2/Views/FavouritesViews.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritesViews.swift
+//  Jokes.V2
+//
+//  Created by Cooper Lawson on 2023-04-19.
+//
+
+import SwiftUI
+
+struct FavouritesViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouritesViews_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouritesViews()
+    }
+}
